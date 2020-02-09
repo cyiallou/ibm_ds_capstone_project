154ce8df2efb36c61088a4448b64e32c8f4cf751
cyiallou <cyiallou@users.noreply.github.com>
2020-02-09 12:33:22 +0100

    fsquare.py class module major update
    - Attribute get_fsquare_data: Now calls the static method
         _check_response_code to display useful api warnings.
         Also some minor bug fixes.
    - New attribute get_all_fsquare_categories: Gets all the
         categories as json and allows to select specific
         venue names from a specific main category.
    - New method _find_str_recur: Too specific. Needs to be recoded
        to become general method. For use within get_all_fsquare_categories.
    - New static method _check_response_code: Checks the API
        response code and outputs useful warnings.

--- fsquare.py
+++ fsquare.py
@@ -14,6 +14,7 @@ from typing import List, Tuple
 
 
 class fsquare():
+
     def __init__(self, client_id, client_secret):
         self.client_id = client_id
         self.client_secret = client_secret
@@ -78,9 +79,9 @@ class fsquare():
         --------
         API call response as a json file.
         """
-        if tp=='cat':
+        if tp == 'cat':
             a = f'&categoryId={search_params["categories"]}'
-        elif tp=='qur':
+        elif tp == 'qur':
             a = f'&query={search_params["query"]}'
         else:
             raise ValueError(f"tp can be either 'cat' or 'qur'. Got {tp}.")
@@ -94,7 +95,6 @@ class fsquare():
                f'&radius={search_params["radius"]}'
                f'&limit={search_params["limit"]}')
         return requests.get(url).json()
-
 
     def get_fsquare_data(self, api_params:dict, queries:List[str], tp:str, coords=[[]], verbose=0) -> Tuple:
         """Calls the Foursquare API for the search strings in queries
@@ -133,11 +133,11 @@ class fsquare():
         """
         # check coords input
         if type(coords)==list:
-            if len(coords)==1 & len(coords[0])==0:
+            if (len(coords) == 1) & (len(coords[0]) == 0):
                 coords = [[api_params['search_params']['latitude'], api_params['search_params']['longitude']]]
             number_of_areas_to_search = len(coords)
         else:
-            if sum(coords[0].shape)!=2:
+            if sum(coords[0].shape) != 2:
                 raise ValueError(f'coords must be of shape (?, 2). Got {coords.shape}.')
             number_of_areas_to_search = coords.shape[0]
 
@@ -146,18 +146,17 @@ class fsquare():
             api_params['search_params']['longitude'] = longlat[0]
             api_params['search_params']['latitude'] = longlat[1]
             for item in queries:  # search for each query
-                if tp=='cat':
+                if tp == 'cat':
                     api_params['search_params']['categories'] = item
-                elif tp=='qur':
+                elif tp == 'qur':
                     api_params['search_params']['query'] = item
                 try:
                     response = self.make_fsquare_api_call(api_params['search_params'], api_params['fixed_search_params'], tp=tp)
                     # display some useful warnings
-                    if response['meta']['code']==429:
-                        warn("API regural calls limit exceeded. Function returned.")
+                    exit_func = self._check_response_code(response['meta']['code'])
+                    if exit_func:
                         return all_fsq_data, venue_ids
-                    if response['meta']['code']==500:  # server error
-                        warn(f"Server error. Response output: {reponse}")
+                    
                     # get the venues
                     venues = response['response']['venues']
                 except:
@@ -175,3 +174,99 @@ class fsquare():
             print(f'Finished processing {number_of_areas_to_search} areas. Found {len(venue_ids)} venues!')
             
         return all_fsq_data
+
+    def get_all_fsquare_categories(self, fixed_api_params:dict, categ_name:str, w:str):
+        """Calls the Foursquare API to get all the venue categories.
+
+        Arguments:
+        ----------
+        fixed_api_params: dict,
+            Holds the API credentials and version.
+
+        categ_name: str,
+            The main category name to search for.
+
+        w: str,
+            String to search in the name of the venues of the main category
+            defined by the argument cat_name.
+
+        Returns:
+        --------
+        all_categories: dict,
+            API raw response: All the venue categories.
+
+        w_categories: list,
+            The category IDs of the venues that have the string w in their name.
+
+        Notes:
+        ------
+        * Look at https://developer.foursquare.com/docs/resources/categories
+          for the category names and identity numbers.
+        """
+        url = ('https://api.foursquare.com/v2/venues/categories'
+               f'?client_id={fixed_api_params["client_id"]}'
+               f'&client_secret={fixed_api_params["client_secret"]}'
+               f'&v={fixed_api_params["version"]}')
+        all_categories = requests.get(url).json()
+
+        exit_func = self._check_response_code(all_categories['meta']['code'])
+        if exit_func:
+            return None
+
+        valid_main_category_names = [item['name'] for item in all_categories['response']['categories']]
+        if categ_name not in valid_main_category_names:
+            raise ValueError(f'String {categ_name} is not a main category name.'
+                             f'Viable names are\n{valid_main_category_names}')
+
+        # get the category names that have the string defined by the argument w
+        w_categories = []
+        for primary in all_categories['response']['categories']:
+            if primary['name'] == categ_name:
+                w_categories = self._find_str_recur(w_categories, primary, w)
+
+        return all_categories, w_categories
+
+    def _find_str_recur(self, out:list, d:dict, w:str) -> list:
+        """Finds w in d recursively and appends the result to out.
+
+        Arguments:
+        ----------
+        out: list,
+            Stores the values from the nested dictionary d that have the
+            string w in the d['categories][i]['name'] value. i is a list
+            index.
+
+        d: dict,
+            Nested dictionary. The whole or part of an API response as in
+            json file format.
+
+        w: str,
+            String to search in the name of the venues.
+
+        Returns:
+        --------
+        out: list,
+            The input list appended with new entries if conditions are met.
+        """
+        if len(d['categories']) != 0:
+            for item in d['categories']:
+                if w.lower() in item['name'].lower():
+                    out.append(item['id'])
+                    self._find_str_recur(out, item, w)
+        else:
+            pass
+        return out
+
+    @staticmethod
+    def _check_response_code(code:int) -> bool:
+        """Returns a warning given the API status code."""
+        exit_func = False
+        if code == 429:
+            warn("API regural calls limit exceeded. Function returned.")
+            exit_func = True
+        elif code == 500:  # server error
+            warn(f"Server error. Response output: {reponse}")
+        else:
+            warn(f"API response status code: {code}")
+        return exit_func
+
